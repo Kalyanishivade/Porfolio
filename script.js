7277d19b4609a50f1b46a032746af22c524fdc3a
Kalyanishivade <kalyanishivade1601@gmail.com>
2026-05-30 11:58:54 +0530

    Implement theme toggle and typing animation
    Added a theme toggle function and a typing animation.

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleTheme() {
+    document.body.classList.toggle("dark");
+}
+
+// Typing animation
+var typed = new Typed("#typing", {
+    strings: [
+        "Full Stack .NET Developer",
+        "Bootstrap UI Designer",
+        "DevExpress XAF Developer",
+        "Learning DevOps 🚀"
+    ],
+    typeSpeed: 60,
+    backSpeed: 40,
+    loop: true
+});
